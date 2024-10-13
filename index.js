0a8b157ddb81df62e6baf2e56ab38fac83a15b0e
NIKI443 <140906614+NIKI443@users.noreply.github.com>
2024-10-13 22:12:03 +0300

    bug fixes

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 const products = document.querySelectorAll(".shelf_product");
-const canvas = document.querySelector(".canvas")
-const cart_btn = document.querySelector(".cart_btn")
+const canvas = document.querySelector(".canvas");
+const cart_btn = document.querySelector(".cart_btn");
 
-const cart = []
+const cart = [];
 let currentDroppable = null;
 
-
-
 function setMargin() {
-  const margin = (window.innerWidth - 200) / 2; 
-  canvas.style.setProperty('--dynamic-margin', `${margin}px`);
+  const margin = (window.innerWidth - 200) / 2;
+  canvas.style.setProperty("--dynamic-margin", `${margin}px`);
 }
-window.addEventListener('load', setMargin);
-window.addEventListener('resize', setMargin);
+window.addEventListener("load", setMargin);
+window.addEventListener("resize", setMargin);
 
+function cartPush() {
+  cart.push(1);
+  if (cart.length >= 3) {
+    cart_btn.style.opacity = "1";
+  }
+}
 
-products.forEach((product)=>{
-  product.addEventListener('mousedown', dragMoveMouse); 
-  product.addEventListener('touchstart', dragMoveTouch); 
-})
+products.forEach((product) => {
+  product.addEventListener("mousedown", dragMoveMouse);
+  product.addEventListener("touchstart", dragMoveTouch);
+});
 
-function  dragMoveTouch(event) {
-  let touch = event.targetTouches[0]
-  let product = event.target
-  let shiftX = touch.clientX - product.getBoundingClientRect().left ;
+function dragMoveTouch(event) {
+  let touch = event.targetTouches[0];
+  let product = event.target;
+  let shiftX = touch.clientX - product.getBoundingClientRect().left;
   let shiftY = touch.clientY - product.getBoundingClientRect().top;
+
   moveAt(touch.pageX, touch.pageY);
   function moveAt(pageX, pageY) {
-    product.style.left = pageX - shiftX + 'px';
-    product.style.top = pageY - shiftY + 'px';
+    product.style.left = pageX - shiftX + "px";
+    product.style.top = pageY - shiftY + "px";
   }
-
+  document.removeEventListener("touchmove", onTouchMove);
   function onTouchMove(event) {
-    let touch = event.targetTouches[0]
+    let touch = event.targetTouches[0];
     moveAt(touch.pageX, touch.pageY);
     product.style.zIndex = 1000;
 
@@ -41,92 +46,96 @@ function  dragMoveTouch(event) {
     product.hidden = false;
 
     if (!elemBelow) return;
-    let droppableBelow = elemBelow.closest('.cart_img');
+    let droppableBelow = elemBelow.closest(".cart_img");
+
+    if (elemBelow == document.body) {
+      document.removeEventListener("touchmove", onTouchMove);
+    }
+
     if (currentDroppable != droppableBelow) {
-      if (currentDroppable) { 
-        leaveDroppable(currentDroppable );
-        
+      if (currentDroppable) {
+        leaveDroppable(currentDroppable);
       }
       currentDroppable = droppableBelow;
-      if (currentDroppable) { 
-        enterDroppable(currentDroppable );
+      if (currentDroppable) {
+        enterDroppable(currentDroppable);
+        product.ontouchend = function () {
+          document.removeEventListener("touchmove", onTouchMove);
+          product.ontouchend = null;
+          product.style.zIndex = 1;
+          cartPush();
+        };
       }
     }
   }
-  document.addEventListener('touchmove', onTouchMove);
 
-  product.ontouchend  = function() {
-    document.removeEventListener('touchmove', onTouchMove);
-    product.ontouchend  = null;
+  document.addEventListener("touchmove", onTouchMove);
+
+  product.ontouchend = function () {
+    document.removeEventListener("touchmove", onTouchMove);
+    product.ontouchend = null;
     product.style.zIndex = 1;
   };
+}
 
-};
-
-products.ondragstart = function() {
+products.ondragstart = function () {
   return false;
 };
 
- function dragMoveMouse(event){
+function dragMoveMouse(event) {
   event.preventDefault();
-  let product = event.target
-  let shiftX = event.clientX - product.getBoundingClientRect().left ;
-  let shiftY = event.clientY - product.getBoundingClientRect().top ;
-  console.log(product.getBoundingClientRect().margin)
+  let product = event.target;
+  let shiftX = event.clientX - product.getBoundingClientRect().left;
+  let shiftY = event.clientY - product.getBoundingClientRect().top;
   product.style.zIndex = 1000;
   moveAt(event.pageX, event.pageY);
   function moveAt(pageX, pageY) {
-    product.style.left = pageX - shiftX + 'px';
-    product.style.top = pageY - shiftY + 'px';
+    product.style.left = pageX - shiftX + "px";
+    product.style.top = pageY - shiftY + "px";
   }
 
   function onMouseMove(event) {
     moveAt(event.pageX, event.pageY);
     product.style.zIndex = 1000;
-    
+
     product.hidden = true;
     let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
     product.hidden = false;
 
     if (!elemBelow) return;
-    let droppableBelow = elemBelow.closest('.cart_img');
-    if (currentDroppable == droppableBelow) {
-      if (currentDroppable) { 
+    let droppableBelow = elemBelow.closest(".cart_img");
+    if (elemBelow == document.body) {
+      document.removeEventListener("mousemove", onMouseMove);
+    }
+    if (currentDroppable != droppableBelow) {
+      if (currentDroppable) {
         leaveDroppable(currentDroppable);
-        console.log(product)
       }
       currentDroppable = droppableBelow;
-      if (currentDroppable) { 
+      if (currentDroppable) {
         enterDroppable(currentDroppable);
+        product.onmouseup = function () {
+          document.removeEventListener("mousemove", onMouseMove);
+          product.onmouseup = null;
+          product.style.zIndex = 1;
+          cartPush();
+        };
       }
     }
-
   }
-  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener("mousemove", onMouseMove);
 
-  product.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    product.ontouchend  = null;
+  product.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    product.onmouseup = null;
     product.style.zIndex = 1;
   };
 }
 
-
 function enterDroppable(elem) {
-  elem.style.background = '#e6e6e677';
-
-
-  cart.push(1)
-  if(cart.length >= 3){
-    console.log(cart_btn.style.display)
-    cart_btn.style.opacity = '1'
-  }
- 
-
+  elem.style.background = "#e6e6e677";
 }
 
-function leaveDroppable(elem ){
-  elem.style.background = '';
-
+function leaveDroppable(elem) {
+  elem.style.background = "";
 }
-
